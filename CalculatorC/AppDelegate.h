83e4e541512f85cfd48319ff62ca63b923800131
Kevin Do <54718041+kevin-dough@users.noreply.github.com>
2019-10-29 09:58:14 -0700

    kevin calculator uploaded

---
+++ CalculatorC/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  CalculatorC
+//
+//  Created by Do, Kevin on 10/15/19.
+//  Copyright © 2019 Do, Kevin. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ CalculatorC/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  CalculatorC
+//
+//  Created by Do, Kevin on 10/15/19.
+//  Copyright © 2019 Do, Kevin. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
